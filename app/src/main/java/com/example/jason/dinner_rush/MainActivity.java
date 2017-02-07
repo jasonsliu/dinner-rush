dc4020e04fc51e044e573eeb625c7820f019224b
Jason Liu <jason.sean.liu@gmail.com>
2017-02-07 13:03:10 -0800

    Added temporary activities for development

--- app/src/main/java/com/example/jason/dinner_rush/MainActivity.java
+++ app/src/main/java/com/example/jason/dinner_rush/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jason.dinner_rush;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void connectButtonPress(View view) {
+        Intent intent = new Intent(this, ConnectActivity.class);
+        startActivity(intent);
+    }
+
+    public void playButtonPress(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void tradeButtonPress(View view) {
+        Intent intent = new Intent(this, TradeActivity.class);
+        startActivity(intent);
     }
 }
